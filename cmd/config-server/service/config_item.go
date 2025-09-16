70dfd1da13f113641bbbbd6ae6828cfaf61a74b1
ambition <39669748+Ambition9186@users.noreply.github.com>
2025-09-16 16:11:58 +0800

    fix: 修复搜索文件配置列表时数量显示错误问题 (#267)

--- cmd/config-server/service/config_item.go
+++ cmd/config-server/service/config_item.go
@@ -492,6 +492,7 @@ func (s *Service) ListConfigItems(ctx context.Context, req *pbcs.ListConfigItems
 		Count:          rp.Count,
 		Details:        rp.Details,
 		ConflictNumber: rp.ConflictNumber,
+		TotalQuantity:  rp.TotalQuantity,
 	}
 	return resp, nil
 }
